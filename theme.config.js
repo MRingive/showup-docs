7f710400de6e0f3b4fd39784ed90ef02dd4e26d8
MRingive <mathiasringive@hotmail.com>
2022-05-26 15:59:58 +0200

    Clean up

--- theme.config.js
+++ theme.config.js
@@ -18,43 +18,10 @@ export default {
       <meta httpEquiv="Content-Language" content="en" />
       <meta name="description" content="The Show Up Club: for procrastinators. By procrastinators." />
       <meta name="og:description" content="The Show Up Club: for procrastinators. By procrastinators." />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
+      <meta name="twitter:site:domain" content="docs.theshowup.club" />
+      <meta name="twitter:url" content="https://docs.theshowup.club/" />
       <meta name="og:title" content="The Show Up Club: for procrastinators. By procrastinators." />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
       <meta name="apple-mobile-web-app-title" content="The Show Up Club" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
   search: true,
